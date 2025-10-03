933f33c493f7f9ebc5829fdb16d5ab6fcc0259b9
Realbob3 <bobrocksx@gmail.com>
2025-10-02 19:58:39 -0600

    Changes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        job job1 = new job();
+        job1_jobtitle = "Software Developer";
+        job1_company = "Valve";
+        job1_startyear = 2017;
+        job1_endyear = 2020;
+
+        job job2 = new job();
+        job2_jobtitle = "Sales representative";
+        job2_company = "Samsung";
+        job2_startyear = 2020;
+        job2_endyear = 2024;
+
+        resume resume1 = new resume();
+        resume1_name = "John Resume";
+
+        resume1_jobs.add(job1);
+        resume1_jobs.add(job2);
+
+        resume1.Display();
     }
 }
