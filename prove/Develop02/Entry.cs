0d5a6caf72ce5fd7c6b6edb9a67c284a07e02660
Realbob3 <bobrocksx@gmail.com>
2025-10-17 20:25:00 -0600

    Fully seperated classes

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,20 @@
+using System;
+public class Entry
+{
+
+    public string _date;
+    public string _promptText;
+    public string _entryText;
+
+    public Entry()
+    {
+
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Date: {_date} - Prompt: {_promptText}");
+        Console.WriteLine($"Response: {_entryText}");
+        Console.WriteLine("-----------------------------------");
+    }
+}
