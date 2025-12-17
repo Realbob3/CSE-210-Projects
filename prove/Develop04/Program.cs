e00d2319af95a83e90eed2bce3b3372036a09488
Realbob3 <bobrocksx@gmail.com>
2025-12-17 08:49:14 -0700

    Please work

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        bool keepRunning = true;
+
+        while (keepRunning)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start breathing activity");
+            Console.WriteLine("  2. Start reflecting activity");
+            Console.WriteLine("  3. Start listing activity");
+            Console.WriteLine("  4. Quit");
+            Console.Write("Select a choice from the menu: ");
+
+            string choice = Console.ReadLine();
+
+            Activity activity = null;
+
+            switch (choice)
+            {
+                case "1":
+                    activity = new BreathingActivity();
+                    break;
+                case "2":
+                    activity = new ReflectionActivity();
+                    break;
+                case "3":
+                    activity = new ListingActivity();
+                    break;
+                case "4":
+                    keepRunning = false;
+                    continue;
+                default:
+                    Console.WriteLine("Invalid selection. Please try again.");
+                    Thread.Sleep(2000);
+                    continue;
+            }
+
+            if (activity != null)
+            {
+                if (activity is BreathingActivity breathing) breathing.Run();
+                else if (activity is ReflectionActivity reflection) reflection.Run();
+                else if (activity is ListingActivity listing) listing.Run();
+            }
+        }
     }
 }
